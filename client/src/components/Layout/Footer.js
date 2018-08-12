ab658206796f4bfd074eb1de59ec4622b8c710d7
Aarick-F <zeflyndesign@gmail.com>
2018-08-12 13:13:44 -0400

    added github icon to footer

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="footer">
       <h1>&copy; SYNTHONIUM {new Date().getFullYear()}</h1>
+      <a href="https://github.com/Aarick-F/synthonium-v2" target="_blank">
+        <i class="fab fa-github-square fa-2x"></i>
+      </a>
     </div>
   );
 }
