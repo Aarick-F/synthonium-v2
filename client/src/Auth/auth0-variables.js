3624ba47e45e1c524c77993d4a6378224e63c2e9
Aarick-F <zeflyndesign@gmail.com>
2018-08-12 13:23:39 -0400

    Test

--- client/src/Auth/auth0-variables.js
+++
@@ -1,7 +0,0 @@
-export const AUTH_CONFIG = {
-  domain: 'aarick-f.auth0.com',
-  clientId: 'Q2O3PpQ5HJ0F9zXPniQhARAsnPIp8Bh6',
-  callbackUrl: 'http://localhost:3000/callback'
-}
-
-//CHANGES WERE MADE??
